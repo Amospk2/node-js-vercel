35d712670373a013ba8e132ff63cad8e7c821a14
Amospk2 <googletraduzpramin@hotmail.com>
2023-01-23 16:36:24 -0300

    change db

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE, 
+    process.env.DB_DBNAME, 
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
