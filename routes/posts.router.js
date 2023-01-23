a77f18b0cdd093984690f4cb4594d974b8661ee7
Amospk2 <googletraduzpramin@hotmail.com>
2023-01-23 16:50:38 -0300

    new routes

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 const postsController = require("../controller/posts.controller")
 
-router.get("/", postsController.list)
-router.get("/:id", postsController.show)
+router.get("/", postsController.getAll)
+router.get("/:id", postsController.getById)
 router.post("/", postsController.create)
 router.put("/:id", postsController.update)
 router.delete("/:id", postsController.delete)
